56e394675539be49d2cd698c22d88c90d363bcb6
Thiago Ferrarezi <thiago.ferrarezi@sumicity.net.br>
2017-12-15 12:20:08 -0200

    correções

--- src/constantes.js
+++ src/constantes.js
@@ -1,31 +1,31 @@
 export const C = {
   URL: {
     ADMINISTRADOR: {
-      BASE: 'http://localhost:5000/administrador/',
-      CADASTROINICIAL: 'http://localhost:5000/administrador/cadastroInicial/',
-      LOGAR: 'http://localhost:5000/administrador/logar'
+      BASE: 'http://10.98.5.51:5000/administrador/',
+      CADASTROINICIAL: 'http://10.98.5.51:5000/administrador/cadastroInicial/',
+      LOGAR: 'http://10.98.5.51:5000/administrador/logar'
     },
     FARMACIA: {
-      BASE: 'http://localhost:5000/farmacia/',
-      VIP: 'http://localhost:5000/farmacia/vip'
+      BASE: 'http://10.98.5.51:5000/farmacia/',
+      VIP: 'http://10.98.5.51:5000/farmacia/vip'
     },
     PLANTAO: {
-      BASE: 'http://localhost:5000/plantao/',
-      DELETE: 'http://localhost:5000/plantao/deletar/',
-      PORDIA: 'http://localhost:5000/plantao/plantoes/dia',
-      PORMES: 'http://localhost:5000/plantao/plantoes/mes'
+      BASE: 'http://10.98.5.51:5000/plantao/',
+      DELETE: 'http://10.98.5.51:5000/plantao/deletar/',
+      PORDIA: 'http://10.98.5.51:5000/plantao/plantoes/dia',
+      PORMES: 'http://10.98.5.51:5000/plantao/plantoes/mes'
     },
     PATROCINADOR: {
-      BASE: 'http://localhost:5000/patrocinador/',
-      VIP: 'http://localhost:5000/farmacia/vip'
+      BASE: 'http://10.98.5.51:5000/patrocinador/',
+      VIP: 'http://10.98.5.51:5000/farmacia/vip'
     },
     PROPAGANDA: {
-      BASE: 'http://localhost:5000/propaganda/',
-      DELETE: 'http://localhost:5000/propaganda/deletar/',
-        PORDIA: 'http://localhost:5000/propaganda/dias/'
+      BASE: 'http://10.98.5.51:5000/propaganda/',
+      DELETE: 'http://10.98.5.51:5000/propaganda/deletar/',
+        PORDIA: 'http://10.98.5.51:5000/propaganda/dias/'
     },
     ENDERECO: {
-      BASE: 'http://localhost:5000/endereco'
+      BASE: 'http://10.98.5.51:5000/endereco'
     }
   }
 }
